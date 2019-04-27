0920dfeab62f824dfa75470bd7b856a63396bc37
proactive-point-1823 <daylanberry@gmail.com>
2019-04-27 02:47:01 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,11 @@
-function forLoop(){
-  var string = ''
+function forLoop(array){
+  var arr = [];
   for (var i = 0; i <25; i++){
     if (i === 1){
-      string += 'I am 1 strange loop. '
+      arr.push('I am 1 strange loop.')
     } else {
-      string += `I am ${i} strange loop `
+      arr.push(`I am ${i} strange loop`)
     }
   }
+  return arr;
 }
