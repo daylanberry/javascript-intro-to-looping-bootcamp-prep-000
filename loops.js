94ec8c327b4877a767989a8ef4109891106ff867
proactive-point-1823 <daylanberry@gmail.com>
2019-04-27 02:44:16 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,4 +1,10 @@
 function forLoop(array){
-  
-  for (var i = 0; i <array.length; i++)
+  var string = ''
+  for (var i = 0; i <array.length; i++){
+    if (i === 1){
+      string += 'I am 1 strange loop.'
+    } else {
+      string += `I am ${i} strange loop`
+    }
+  }
 }
